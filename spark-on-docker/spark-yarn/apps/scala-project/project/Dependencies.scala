8a00fcb0257384f88207530e63d813f3dfea9807
José Nunes <josenunesra@gmail.com>
2023-09-19 00:26:11 +0000

    feat: updates spark base

--- spark-on-docker/spark-yarn/apps/scala-project/project/Dependencies.scala
+++ spark-on-docker/spark-yarn/apps/scala-project/project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   val scalaTest      = "org.scalatest"    %% "scalatest"  % "3.0.8"
-  val sparkCore      = "org.apache.spark" %% "spark-core" % "3.4.0"
-  val sparkSql       = "org.apache.spark" %% "spark-sql"  % "3.4.0"
+  val sparkCore      = "org.apache.spark" %% "spark-core" % "3.5.0"
+  val sparkSql       = "org.apache.spark" %% "spark-sql"  % "3.5.0"
 }
